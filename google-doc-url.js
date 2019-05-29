b96e76dc5e873ee0671c537b8d922cad5af53276
fabianm536 <fabianm536@gmail.com>
2019-05-29 09:42:53 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ilHZq-rLRK586dq_BHwja7tWA3qQ0Rf8RA8LgQbreNc/';
